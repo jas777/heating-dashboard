fd8a25da59ced58d69ad5e9c54d282417e605031
jas777 <jas.czeszejko@wp.pl>
2021-12-03 14:57:57 +0100

    Added tests

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {useAxios} from "use-axios-client";
 
 function App() {
 
-    const {data, error, loading} = useAxios({
+    const { data, error, loading } = useAxios({
         url: `${process.env.REACT_APP_BASE_URL}/config`
     });
 
     let toDisplay = (!data || loading) ? <li><p>Pobieranie danych...</p></li> : data.heaters.map(h => {
-        return <li>{h.id} -> {h.name}</li>
+        return <li key={h.name}>{h.id} -> {h.name}</li>
     })
 
     return (
@@ -17,7 +17,7 @@ function App() {
             <h1 className='text-5xl m-6 font-bold text-indigo-600'>Sterownik ogrzewania</h1>
             <div className='mx-6 my-4'>
                 <p className='text-3xl font-semibold'>Grzejniki</p>
-                <ul>{toDisplay}</ul>
+                <ul data-testid='heaters'>{toDisplay}</ul>
             </div>
         </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const mock = new MockAdapter(axios)
+
+beforeAll(() => {
+    mock.reset()
+})
+
+test('Tries to fetch data from API', async () => {
+
+    mock.onGet(`${process.env.REACT_APP_BASE_URL}/config`)
+        .reply(200, {heaters: [{id: 1, name: 'Salon'}]})
+
+    const { queryByText, getByText } = render(<App />)
+    expect(queryByText(/Pobieranie danych.../i)).toBeInTheDocument()
+    expect(queryByText(/Salon/i)).not.toBeInTheDocument()
+
+    await waitFor(() => getByText(/Salon/i))
+    expect(queryByText(/Pobieranie danych.../i)).not.toBeInTheDocument()
+    expect(queryByText(/Salon/i)).toBeInTheDocument()
+
+})
