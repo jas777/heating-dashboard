748828251cb63bf77fcdf00d9a55f62ad30b2658
jas777 <jas.czeszejko@wp.pl>
2021-12-03 13:46:48 +0100

    fetching config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useAxios} from "use-axios-client";
 
 function App() {
 
-  const data = () => {};
+    const {data, error, loading} = useAxios({
+        url: `${process.env.REACT_APP_BASE_URL}/config`
+    });
 
-  return (
-    <div className='flex flex-col'>
-      <h1 className='text-5xl m-6 font-bold text-indigo-600'>Sterownik ogrzewania</h1>
-      <div className='mx-6 my-4'>
-        <p className='text-3xl font-semibold'>Grzejniki</p>
-        <ul></ul>
-      </div>
-    </div>
-  );
+    let toDisplay = (!data || loading) ? <li><p>Pobieranie danych...</p></li> : data.heaters.map(h => {
+        return <li>{h.id} -> {h.name}</li>
+    })
+
+    return (
+        <div className='flex flex-col'>
+            <h1 className='text-5xl m-6 font-bold text-indigo-600'>Sterownik ogrzewania</h1>
+            <div className='mx-6 my-4'>
+                <p className='text-3xl font-semibold'>Grzejniki</p>
+                <ul>{toDisplay}</ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
